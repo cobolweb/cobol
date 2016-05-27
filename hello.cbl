d582cd24709a3d7e298d42342fabd2f8b75440ec
cobolweb <jgruendike@outlook.com>
2016-05-26 17:35:04 -0700

    Create hello.cbl

---
+++ hello.cbl
@@ -0,0 +1,18 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID.     HELLOWORLD.
+
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+
+       DATA DIVISION.
+       FILE SECTION.
+
+       PROCEDURE DIVISION.
+
+       MAIN-LOGIC SECTION.
+       BEGIN.
+           DISPLAY " "
+           DISPLAY "Hello world!"
+           STOP RUN.
+       MAIN-LOGIC-EXIT.
+           EXIT.
